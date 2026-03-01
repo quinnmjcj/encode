b11a643cfb5c24ab97e4d18b25e27a4fdb0d6a34
quinnmjcj <46435100+quinnmjcj@users.noreply.github.com>
2026-03-01 09:20:33 -0800

    Update RetitleMkv.ps1 to current

--- RetitleMkv.ps1
+++ RetitleMkv.ps1
@@ -1,29 +1,48 @@
-﻿param (
+#!/usr/bin/pwsh
+
+param (
  [Parameter(Mandatory = $true, Position = 0)]
  [string]$input_name,
  [Parameter(Mandatory = $false)]
  [string]$encode="",
  [switch]$only=$false,
- [switch]$none=$false
+ [switch]$none=$false,
+ [string]$group=""
+
 )
 
 Import-Module MediaUtils
 
-$file = gci $input_name
+$file = Get-ChildItem $input_name
 
 $info = Get_Media_Info $file.FullName
 
 $base = $file.BaseName
 $height = Get_video_height $info
+$oheight = Get_video_original_height $info
 $width = Get_video_width $info
 $hdr = Is_Display_BT2020 $info
+$dv_type = Video_dv_type $info
+
+#Write-Host $height $oheight $width $hdr $dv_type
 
 if (($height -eq 2160) -or ($width -eq 3840)) {
     if ( !$hdr ) {
-        $type = "4K SDR"
+        if (($oheight -eq 1080) -and ($width -eq 1920)) {
+            $type = "1080p"
+        }
+        elseif ($dv_type -eq "05.06") {
+            $type = "4K_dv5"
+        }
+        else {
+            $type = "4K SDR"
+        }
     }
     else {
         $type = "4K"
+        if ($dv_type -match "\d(\d)\.\d\d") {
+            $type += "_dv" + $matches[1]
+        }
     }
 }
 else {
@@ -61,7 +80,26 @@ elseif ($base -match "(.+)\.(web)") {
 }
 else {
     $comp = ""
-    $name = $base
+    if ($base -match "(.+) \(Bluray-1080p\)") {
+        $name = $matches[1]
+    }
+    elseif ($base -match "(.+) \(Bluray-2160p( .*)?\)") {
+        $name = $matches[1]
+    }
+    elseif ($base -match "(.+) \(WEBDL-\d\d\d\dp( .*)?\)") {
+        $comp = "web"
+        $name = $matches[1]
+    }
+    elseif ($base -match "(.+) \(WEBRip-\d\d\d\dp( .*)?\)") {
+        $comp = "webrip"
+        $name = $matches[1]
+    }
+     elseif ($base -match "(.+) \(SDTV\)") {
+        $name = $matches[1]
+    }
+    else {
+        $name = $base
+    }
 }
 
 if ($encode -ne "") {
@@ -85,7 +123,7 @@ if ((! $none)) {
 }
 #Write-Host $base " - " $title
 
-$s = [string]$file.FullName 
+$s = [string]$file.FullName
 $t = Get_Title $info
 $t2 = $title + " /"
 if ($t.Length -gt $t2.Length) {
@@ -94,9 +132,10 @@ if ($t.Length -gt $t2.Length) {
 else {
     $comp = $title
 }
+if (($group -ne "") -and ($group -ne "none")) {
+    $title += " [$group]"
+}
 if (($t -ne $title)) {
     Write-Host("Set new title to '$title' (from '$t')")
     Change_title  $s $title
 }
-
-
